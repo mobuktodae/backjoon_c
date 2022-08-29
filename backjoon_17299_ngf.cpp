13fdcb933ee25d1f9b121722b4937e6d1ec9c2c0
mobuktodae <87495422+mobuktodae@users.noreply.github.com>
2022-08-29 20:03:26 +0900

    17299 문제 풀이 수정

--- backjoon_17299_ngf.cpp
+++ backjoon_17299_ngf.cpp
@@ -1,9 +1,57 @@
 #include<iostream>
-#include<queue>
+#include<stack>
+#include<vector>
+#define MAX 1000000
 
-using namespace std;
+int count[MAX] = { 0 };
+
+typedef struct num {
+	int data;
+	bool check;
+}Num;
 
 int main()
 {
-	
+	int n;
+	std::cin >> n;
+	std::vector<Num*> v;
+	std::stack<Num*> nonfind;
+	for (int i = 0; i < n; i++)
+	{
+		int number;
+		std::cin >> number;
+		Num* curr = (Num*)malloc(sizeof(Num));
+		curr->check = false;
+		curr->data = number;
+		v.push_back(curr);
+		count[number]++;
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (nonfind.empty())
+			nonfind.push(v[i]);
+		else
+		{
+			if (count[nonfind.top()->data] > count[v[i]->data])
+				nonfind.push(v[i]);
+			else {
+				while (!nonfind.empty() && count[nonfind.top()->data] < count[v[i]->data]) {
+					nonfind.top()->data = v[i]->data;
+					nonfind.top()->check = true;
+					nonfind.pop();
+				}
+				nonfind.push(v[i]);
+			}
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (v[i]->check)
+			std::cout << v[i]->data << ' ';
+		else
+			std:: cout << -1 << ' ';
+	}
+
+	return 0;
 }

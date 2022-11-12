2c8feef79238bf7b8f3f3903b1800b3fe62054cc
mobuktodae <now0104@naver.com>
2022-11-13 01:49:43 +0900

    11726 풀이 추가

---
+++ backjoon_11726.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#define MAX 1001
+
+long long arr[MAX] = { 0 };
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i <= n; i++)
+		arr[i] = (arr[i - 1] + arr[i - 2])%10007;
+	printf("%lld", arr[n]);
+}
